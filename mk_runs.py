9722eddee1d99cb335485dad36b2a78f96f4864e
Peter Teuben <teuben@gmail.com>
2024-07-26 11:06:13 -0400

    add

--- mk_runs.py
+++ mk_runs.py
@@ -10,16 +10,25 @@ project="2024-S1-US-16"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
-on["A262_1"] =  [ 119785, 119786,]
+on["A262_1"] =  [ 119785, 119786, 120088, 120102,]
+
+on["A262_2"] =  [ 120096, 120104,]
+
+on["A262_3"] =  [ 120100, 120106,]
+
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 pars1["A262_1"] = ""
+pars1["A262_2"] = ""
+pars1["A262_3"] = ""
 
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
 pars2["A262_1"] = "bank=0 pix_list=-13"
+pars2["A262_2"] = "bank=0 pix_list=-13"
+pars2["A262_3"] = "bank=0 pix_list=-13"
 
 #        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
 pars3 = {}
