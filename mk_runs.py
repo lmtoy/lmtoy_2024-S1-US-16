9c81bc48deb34693f24ee21ddf6a543be9dba5a9
Peter Teuben <teuben@gmail.com>
2024-08-25 13:30:43 +0000

    upd

--- mk_runs.py
+++ mk_runs.py
@@ -10,15 +10,17 @@ project="2024-S1-US-16"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
+
 on["A262_1"] =  [ 119785, 119786, 120088, 120102, 120275,]
 
-on["A262_2"] =  [ 120096, 120104,-120279, 120355,]
+on["A262_2"] =  [ 120096, 120104,-120279, 120355, 120454,]
 
-on["A262_3"] =  [ 120100, 120106, 120359, 120361,]
+on["A262_3"] =  [ 120100, 120106, 120359, 120361, 120458, 120466, 120533, 120535, 120537, 120539, 120541,]
 
-on["A262_4"] =  [ 120367, 120369,]
+on["A262_4"] =  [ 120367, 120369, 120462, 120564, 120566, 120568, 120570, 120572, 120574, 120576, 120580, 120582, \
+                  120817, 120819, 120821, 120823, 120825,]
 
-
+# parameters for the first pass of the pipeline (restart=1 is automatically enforced here)
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
