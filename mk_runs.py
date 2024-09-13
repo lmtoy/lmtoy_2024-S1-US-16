fa4c03a79202f385a4a1e1bc9ca41445bab85130
LMT Helpdesk <mwatstein@umass.edu>
2024-09-13 15:27:14 +0000

    QA in progress - pushing specifically for archive upload by 09/17/2024

--- mk_runs.py
+++ mk_runs.py
@@ -15,19 +15,19 @@ on["A262_1"] =  [ 119785, 119786, 120088, 120102, 120275,]
 
 on["A262_2"] =  [ 120096, 120104,-120279, 120355, 120454,]
 
-on["A262_3"] =  [ 120100, 120106, 120359, 120361, 120458, 120466, 120533, 120535, 120537, 120539, 120541,]
+on["A262_3"] =  [ 120100, 120106, 120359, 120361, 120458, 120466, -120533, 120535, 120537, 120539, 120541,]
 
-on["A262_4"] =  [ 120367, 120369, 120462, 120564, 120566, 120568, 120570, 120572, 120574, 120576, 120580, 120582, \
+on["A262_4"] =  [ 120367, 120369, -120462, 120564, 120566, 120568, 120570, 120572, 120574, 120576, 120580, 120582, \
                   120817, 120819, 120821, 120823, 120825,]
 
 # parameters for the first pass of the pipeline (restart=1 is automatically enforced here)
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1["A262_1"] = "extent=240"
-pars1["A262_2"] = "extent=240"
-pars1["A262_3"] = "extent=240"
-pars1["A262_4"] = "extent=240"
+pars1["A262_1"] = "extent=240 qagrade=3"
+pars1["A262_2"] = "extent=240 qagrade=3"
+pars1["A262_3"] = "extent=240 qagrade=3"
+pars1["A262_4"] = "extent=240 qagrade=3"
 
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
